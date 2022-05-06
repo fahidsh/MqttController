a489a15b51113c736de0aa223d8d4473bcf7649f
Fahid Shehzad <git@shehzad.eu>
2022-05-07 00:51:25 +0200

    Zwei Motoren Program Kompakt version

--- main.cpp
+++ main.cpp
@@ -6,152 +6,82 @@
 
 #include "mbed.h"
 
-#ifndef SLEEP_TIME
-#define SLEEP_TIME 100ms
-#endif
-#ifndef SLEEP_TIME_BETWEEN_STEPS
-#define SLEEP_TIME_BETWEEN_STEPS 500ms
-#endif
+#define SLEEP_TIME 50ms
+#define MOTOR_SCHRITT 2ms
 
-#ifndef DEBUG_LOG
-// use the following line while Debugging
-#define DEBUG_LOG log_message
-// use the following line in Production
-//#define DEBUG_LOG //
-#endif
-
+// Motor 1 ist auf C0 bis C3 angeschlossen
 PortOut motor1(PortC, 0b1111);
+// Motor 2 ist auf C4 bis C7 angeschlossen
 PortOut motor2(PortC, 0b11110000);
+// Array für Motorlauf nach Links/Normal
 int motorlauf_links[] = {0b0001, 0b0011, 0b0010, 0b0110,
                          0b0100, 0b1100, 0b1000, 0b1001};
+// Array für Motorlauf nach Recht/Umgekehrt
 int motorlauf_rechts[] = {0b1001, 0b1001, 0b1000, 0b1100, 0b0100,
                           0b0110, 0b0010, 0b0011, 0b0001};
+
+// controll variable
 int position = 0;
+// Variable um die Laufrichtung zu setzen
 volatile int motor_richtung = 1; // -1 für Rückwarts, 0 zum stoppen
 
-#define GEDRUCKT 1
-// Taster 1,2,3 auf Sturmboard
+// Taster 1,2,3 auf Sturmboard, alle als Interrupts mit PullDown Modus
 InterruptIn taster1(PA_1, PullDown);
 InterruptIn taster2(PA_6, PullDown);
 InterruptIn taster3(PA_10, PullDown);
 
+// Zwei Threads, jeweils für beide Motoren
 Thread motor1_thread;
 Thread motor2_thread;
 
-/**
-********************************************************
-
-********************************************************
-*/
-void log_message(int, const char *, ...);
-/**
-********************************************************
-
-********************************************************
-*/
+// Kontroll Methode für Motor 1
 void motor1_controller() {
-  Timer t;
-  t.start();
-  int time_s = 0;
-
   while (true) {
-    long time_ms = t.elapsed_time().count() / 1000;
+    // nur was tun wenn motor_richtung nicht gleich null ist.
     if (motor_richtung != 0) {
+      // array Wert an die Pins von Port schicken
       motor1 = motorlauf_links[position % 8];
+      // position um erhöhen/mindern
       position += motor_richtung;
-      if (time_ms / 1000 >= time_s + 2) {
-        DEBUG_LOG(0, "pos1: %d, %d", time_s, position);
-        time_s = time_ms / 1000;
-      }
     }
-    ThisThread::sleep_for(2ms);
+    ThisThread::sleep_for(MOTOR_SCHRITT);
   }
 }
 
+// Kontroll Methode für Motor 2
 void motor2_controller() {
-  Timer t;
-  t.start();
-  int time_s = 0;
-
   while (true) {
-    long time_ms = t.elapsed_time().count() / 1000;
     if (motor_richtung != 0) {
+      // array wert an die Pins schicken, um 4 bits verschieben
       motor2 = motorlauf_rechts[position % 8] << 4;
-      if (time_ms / 1000 >= time_s + 2) {
-        DEBUG_LOG(0, "pos2: %d, %d", time_s, position);
-        time_s = time_ms / 1000;
-      }
     }
-    ThisThread::sleep_for(2ms);
+    ThisThread::sleep_for(MOTOR_SCHRITT);
   }
 }
 
-bool init() { return true; }
-
+// setzt Variable motor_richtung = 1, Motore laufen normal
 void motor_lauf_vorwart() { motor_richtung = 1; }
+// setzt Variable motor_richtung = -1, Motore laufen Rückwart
 void motor_lauf_ruckwart() { motor_richtung = -1; }
+// setzt Variable motor_richtung = 0, Motore stoppen
 void motor_stop() { motor_richtung = 0; }
 
 // main() runs in its own thread in the OS
 int main() {
+
+  // Threads für beide Motoren starten
   motor1_thread.start(motor1_controller);
   motor2_thread.start(motor2_controller);
 
+  // Interrupt von taster1
   taster1.rise(&motor_lauf_vorwart);
+  // Interrupt von taster2
   taster2.rise(&motor_lauf_ruckwart);
+  // Interrupt von taster3
   taster3.rise(&motor_stop);
+
+  // nichts zu tun, einfach in endloss Schleife bleiben
   while (true) {
-    ThisThread::sleep_for(50ms);
+    ThisThread::sleep_for(SLEEP_TIME);
   }
 }
-
-/**
-********************************************************
-
-
-
-
-
-
-
-
-
-
-
-********************************************************
-*/
-
-void log_message(int sender = 99, const char *message_with_args = "", ...) {
-  va_list arg;
-  va_start(arg, message_with_args);
-  // max length of message, printf style
-  char message[250];
-  // take the message into char array
-  vsprintf(message, message_with_args, arg);
-  va_end(arg);
-  // save the message as string (optional)
-  // std::string message_string = message;
-
-  switch (sender) {
-  case 110 ... 199: // supposed to be DEBUG messages
-    printf("DEBUG: %s\n", message);
-    break;
-  case 210 ... 299: //
-    printf("INFO: %s\n", message);
-    break;
-  case 310 ... 399:
-    printf("ERROR: %s\n", message);
-    break;
-  case 410 ... 499:
-    printf("FETAL: %s\n", message);
-    break;
-  default:
-    printf("%s\n", message);
-  }
-}
-
-/**
-********************************************************
-
-********************************************************
-*/
